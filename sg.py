86f20957917b089039b7002d29a3d4c147a7b06b
elsabergquist <elsa.bergquist@student.tabyenskilda.se>
2022-04-01 11:34:45 +0200

    l

--- sg.py
+++ sg.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 from SwedishWordle import *
 
 layout = [
-    [sg.Text('Hej och välkomna till vår wordguesser!, Hur många bokstäver vill du att ordet ska bestå av?'), sg.InputText(key="bokstaver")],
-    [sg.Button("gissa", key="gissing")],
+    [sg.Text('Hej och välkomna till vår wordguesser!, Vill du starta ett nytt spel?'), sg.Button("ja, starta nytt spel", key="get_new")],
+    [sg.InputText(key="gissning")],
     [sg.Text("...", key="info")]]
 
 window = sg.Window("Wordguesser", layout)
@@ -14,6 +14,17 @@ while True:
     # End program if user closes window
     if event == sg.WIN_CLOSED:
         break
+
+    if event == "get_new":
+        response = Start_new_game(self, word_length = 5)
+        message = f'{response}'
+        window["gissning"].update(visible = False)
+        window["info"].update(message)
+    
+    if event == "gissning":
+        response = Guess(self, word_guess)
+        message = f'{response}'
+        window["info"].update(message)
 
 
 window.close()
