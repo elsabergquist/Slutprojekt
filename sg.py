5d52f8a4694c3d81e76d5df5b51fac24e699a7bd
elsabergquist <elsa.bergquist@student.tabyenskilda.se>
2022-05-03 10:49:07 +0200

    m

--- sg.py
+++ sg.py
@@ -4,31 +4,29 @@ import SwedishWordle
 
 sg.theme('DarkTeal6')
 
+sz=(70,70)
+col1=[[sg.Text("...", key = "lista",background_color='Teal', size=sz)]]
+col2=[[sg.Text("...", key = "ord", background_color='Teal', size=sz),]]
+
+
 layout1 = [
     [sg.Text('Hej och välkomna till vår wordguesser!, Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! Vill du starta ett nytt spel?'), sg.Button("ja, starta nytt spel", key="get_new")],
     [sg.Text('När du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. 0 betyder att bokstaven inte finns i ordet, 1 betyder att bokstaven är rätt men på fel plats, 2 betyder att bokstaven är rätt och på rätt plats')]]
     
 layout2 = [
     [sg.Input(key = 'gissning'), sg.Button('gissa', key='gissa')],
-    [sg.Text("...", key = "lista")]
-    ]
-
-layout3 = [
-    [sg.Input(), sg.Button('gissa', key='gissa2')],
-]
-layout4 = [
-    [sg.Input(), sg.Button('gissa', key='gissa3')],
-]
+    [sg.Column(col1, element_justification='c'), sg.Column(col2, element_justification='c')]]
 
 layout = [
-    [sg.Column(layout1, key = 'COL1'), sg.Column(layout2, visible = False, key = 'COL2'), sg.Column(layout3, visible = False, key = 'COL3')]
-]
+    [sg.Column(layout1, key = 'COL1'), sg.Column(layout2, visible = False, key = 'COL2')]]
 
-window = sg.Window("Wordguesser", layout)
+window = sg.Window("Wordguesser", layout, size = (1000, 500))
 
 layout = 1
 
 tidigare_gissningar = []
+tidigare_ord = []
+
 
 while True:
     event, values = window.read()
@@ -48,35 +46,8 @@ while True:
         new_guess = game.Guess(guess)
         message = f'{new_guess}'
         tidigare_gissningar.append(message)
+        tidigare_ord.append(guess)
         window["lista"].update(tidigare_gissningar)
-        
-    
-    
-    # if event == "gissa2":
-        #guess2 = game.Guess('gissa')
-        #message = f'{guess2}'
-        #layout = layout + 1 
-        #window[f'COL{layout}'].update(visible = True)
-        #window["info"].update(message)
-
-    #if event == "gissa3":
-        #guess3 = game.Guess('gissa')
-        #message = f'{guess3}'
-        #window["info"].update(message)
-        
-
-   # if guess == result:
-    #    correct = True
-    #    break
-
-    #if correct:
-    #    sg.popup('You win!')
-
-    
-
-
+        window["ord"].update(tidigare_ord)
 
 window.close()
-
-
-#window[f'COL{layout}'].update(visible = False) layout = layout + 1 if layout < 3 else 1 window[f'COL{layout}'].update(visible = True)
