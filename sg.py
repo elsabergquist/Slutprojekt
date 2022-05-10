a3b9c6ca6867929d79d7a2db1c3776232b10c748
elsabergquist <elsa.bergquist@student.tabyenskilda.se>
2022-05-10 12:54:07 +0200

    push

--- sg.py
+++ sg.py
@@ -5,29 +5,31 @@ import SwedishWordle
 sg.theme('DarkTeal6')
 
 
-sz=(70,70)
+sz=(45,30)
 col1=[[sg.Text( "...", key = "lista",background_color='Teal', size=sz)]]
 col2=[[sg.Text("...", key = "ord", background_color='Teal', size=sz),]]
 col3=[[sg.Text("...", key = "ord", background_color='Teal', size=sz),]]
 
 
 layout1 = [
-    [sg.Text('Hej och välkomna till vår wordguesser!, Vill du starta ett nytt spel?'), sg.Button("ja, starta nytt spel", key="get_new")],
-    [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på!\nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet.\n0 betyder att bokstaven inte finns i ordet, 1 betyder att bokstaven är rätt men på fel plats, 2 betyder att bokstaven är rätt och på rätt plats')]]
+    [sg.Text('Hej och välkomna till vår wordguesser!', font = 'Franklin 26')],
+    [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! \nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. \n0 betyder att bokstaven inte finns i ordet\n1 betyder att bokstaven är rätt men på fel plats \n2 betyder att bokstaven är rätt och på rätt plats', font = 'Franklin 17')],
+    [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20'), sg.Button("ja, starta nytt spel", font = 'Franklin 20', key="get_new")]
+    ]
     
 layout2 = [
     [sg.Input(key = 'gissning'), sg.Button('gissa', key='gissa')],
     [sg.Column(col1, element_justification='c'), sg.Column(col2, element_justification='c')]]
 
 layout3 = [
-    [sg.Text('Grattis! du vann!')],
+    [sg.Text('Grattis! du vann!') ],
     [sg.Text('Vill du spela igen?'), sg.Button('ja!', key = 'kör_igen')],
     [sg.Column(col3, element_justification='c')]]
 
 layout = [
     [sg.Column(layout1, key = 'COL1'), sg.Column(layout2, visible = False, key = 'COL2'), sg.Column(layout3, visible = False, key = 'COL3')]]
 
-window = sg.Window("Wordguesser", layout, size = (1200,300))
+window = sg.Window("Wordguesser", layout, size = (800,300))
 
 
 layout = 1
