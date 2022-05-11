2fe3cdbc9511fb4e7bfc7b522e51b013532d592d
Ellbry03 <58473108+Ellbry03@users.noreply.github.com>
2022-05-11 16:08:41 +0200

    nytt

--- sg.py
+++ sg.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import SwedishWordle  
 import io
+import highscorelista
 
 sg.theme('Reddit')
 
@@ -15,30 +16,41 @@ col2=[[sg.Text('Ord', font = fs)],
 [sg.Text("...", key = "ord", text_color = tc, justification = 'center', font = 'Franklin 20', background_color='blue', size=sz)]]
 col3=[[sg.Text('Antal gissningar', font = fs)],
 [sg.Text("...",  key = "antal", text_color = tc, justification = 'center', font = 'Franklin 50', background_color='blue', size = (20,5))]]
+col4=[[sg.Text("...", key = "score", text_color = tc, justification = 'center', font = 'Franklin 50', background_color='blue', size = (15,10))]]
 
 
 layout1 = [
     [sg.Text('Hej och välkomna till vår wordguesser!', font = 'Franklin 26', justification = 'center')],
     [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! \nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. \n0 betyder att bokstaven inte finns i ordet\n1 betyder att bokstaven är rätt men på fel plats \n2 betyder att bokstaven är rätt och på rätt plats', font = 'Franklin 17', justification = 'center')],
-    [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20', justification = 'center'), sg.Button("Ja, starta nytt spel", font = 'Franklin 20', key="get_new")]
+    [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20', justification = 'center'), sg.Button("Ja, starta nytt spel", font = 'Franklin 20', key="get_new"), sg.Button('Highscore', font = 'Franklin 20', key='high')]
+    
     ]
     
 layout2 = [
-    [sg.Input(key = 'gissning' , font = 'Franklin 20'), sg.Button('gissa', font = 'Franklin 20', key='gissa')],
+    [sg.Input(key = 'gissning' , font = 'Franklin 20'), 
+    sg.Button('gissa', font = 'Franklin 20', key='gissa')],
     [sg.Text("...", key= "varning", font = fs)],
     [sg.Column(col1 , element_justification='c'), sg.Column(col2, element_justification='c'), sg.Column(col3, element_justification= 'c')]]
 
 layout3 = [
     [sg.Text('Grattis! du vann!', font = 'Franklin 26') ],
-    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), sg.Button('ja!', key = 'kör_igen', font = 'Franklin 26')],
+    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
+    sg.Button('ja!', key = 'kör_igen', font = 'Franklin 26')],
     ]
 
 layout4 = [
-    [sg.Text('Du har max 5 gissningar', font = fs)],   [sg.Text('Vill du spela igen?', font = 'Franklin 26'), sg.Button('ja!', key ="kör_igen2", font = 'Franklin 26')],
+    [sg.Text('Du har max 5 gissningar', font = fs)],   
+    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
+    sg.Button('ja!', key ="kör_igen2", font = 'Franklin 26')],
     ]
 
+layout5 = [
+    [sg.Text('Highscore listan', font = fs)], 
+    [sg.Column(col4, element_justification=('c')), 
+    sg.Button('Tillbaka till startsidan', key ="tillbaka", font= 'Frankline 20' )]]
+
 layout = [
-    [sg.Column(layout1, key = 'COL1'), sg.Column(layout2, visible = False, key = 'COL2'), sg.Column(layout3, visible = False, key = 'COL3'), sg.Column(layout4, visible = False, key = 'COL4')]]
+    [sg.Column(layout1, key = 'COL1'), sg.Column(layout2, visible = False, key = 'COL2'), sg.Column(layout3, visible = False, key = 'COL3'), sg.Column(layout4, visible = False, key = 'COL4'),  sg.Column(layout5, visible = False, key = 'COL5')]]
 
 window = sg.Window("Wordguesser", layout, size=(800,300) )
 
@@ -78,9 +90,17 @@ while True:
         game = SwedishWordle.Game(5)
         window.Refresh()
 
-    
-       
+    if event == "tillbaka":
+        window[f'COL{layout}'].update(visible = False)
+        layout = 1
+        window[f'COL{layout}'].update(visible = True)
 
+
+    if event == "high":
+        layout = 1
+        window[f'COL{layout}'].update(visible = False)
+        layout= layout + 4
+        window[f'COL{layout}'].update(visible = True)
 
     if event == "gissa":
         guess = values['gissning']#hämta från inputen
@@ -108,9 +128,12 @@ while True:
                 window[f'COL{layout}'].update(visible = False)
                 layout = layout + 1
                 window[f'COL{layout}'].update(visible = True)
-#nytt
-                with io.open("highscorelista.py", "w", encoding= 'utf-8') as hs:
-                    hs.write(str(antal_gissningar))
+
+                highscore = highscorelista.pick(antal_gissningar)
+                window["antal"].update(antal_gissningar)
+                print(highscore)
+                window["score"].update(highscore)
+
 
         except ValueError:
             felmeddelande = "Någonting gick fel"
