b77161c97c6a7c7982d2e16a6f9e9aeba8ec6eed
elsabergquist <elsa.bergquist@student.tabyenskilda.se>
2022-05-17 09:43:17 +0200

    m

--- layout.py
+++ layout.py
@@ -1,23 +1,82 @@
 
 import PySimpleGUI as sg
 
+def design():
+    sg.theme('Reddit')
+    sz=(20,30) #size
+    fs = 'Frankline 20' #fontsize
+    tc= 'white' #text color
+    return sz,fs,tc
 
-class Gui:
-    def design_startsida(ds):      
+def layout_all(fs, col1, col2, col3, col4, col5):
+    layout_startsida = layout_start()
+    layout_spelsida = layout_spel(fs, col1, col2, col3)
+    layout_vinstsida = layout_vinst()
+    layout_maxgissningar = layout_max(fs)
+    layout_highscorelista = layout_high(fs, col4, col5)  
+    layout = [[sg.Column(layout_startsida, key = 'COL1'), sg.Column(layout_spelsida, visible = False, key = 'COL2'), sg.Column(layout_vinstsida, visible = False, key = 'COL3'), sg.Column(layout_maxgissningar, visible = False, key = 'COL4'),  sg.Column(layout_highscorelista, visible = False, key = 'COL5')]]
+    window = sg.Window('Wordle', layout, size=(800,400))
+    return window
 
-        
-        layout = [
-            [sg.Text('Hej och välkomna till Wordle!', font = 'Franklin 26', justification = 'center')],
-            [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! \nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. \n0 betyder att bokstaven inte finns i ordet\n1 betyder att bokstaven är rätt men på fel plats \n2 betyder att bokstaven är rätt och på rätt plats', font = 'Franklin 17', justification = 'center')],
-            [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20', justification = 'center'), 
-            sg.Button('Ja, starta nytt spel', font = 'Franklin 20', key='starta_spel_button'), 
-            sg.Button('Highscore', font = 'Franklin 20', key='highscore_button')]
-            ]    
+def columner(sz, fs, tc):
+    col1=[[sg.Text('Ledtråd', font = fs)],
+[sg.Text( '...', key = 'lista', text_color = tc, justification = 'center', font = 'Franklin 20', background_color='blue', size=sz)]]
+    col2=[[sg.Text('Ord', font = fs)],
+[sg.Text('...', key ='ord', text_color = tc, justification = 'center', font = 'Franklin 20', background_color='blue', size=sz)]]
+    col3=[[sg.Text('Antal gissningar', font = fs)],
+[sg.Text('...',  key = 'antal', text_color = tc, justification = 'center', font = 'Franklin 50', background_color='blue', size = (20,5))]]
+    col4=[[sg.Text('Vinstgissning:', font = fs)],[sg.Text('...', key = 'rätt_ord', text_color = tc, justification = 'center', font = fs, background_color='blue', size = (15,20))]]
+    col5=[[sg.Text('Antal gissningar', font = fs)],[sg.Text('...', key = 'score', text_color = tc, justification = 'center', font = fs, background_color='blue', size = (15,20))]]
+    return col1,col2,col3,col4,col5
 
-        window = sg.Window('Window Title', layout)    
+def layout_start():
+    layout_startsida = [
+    [sg.Text('Hej och välkomna till Wordle!', font = 'Franklin 26', justification = 'center')],
+    [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! \nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. \n0 betyder att bokstaven inte finns i ordet\n1 betyder att bokstaven är rätt men på fel plats \n2 betyder att bokstaven är rätt och på rätt plats', font = 'Franklin 17', justification = 'center')],
+    [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20', justification = 'center'), 
+    sg.Button('Ja, starta nytt spel', font = 'Franklin 20', key='starta_spel_button'), 
+    sg.Button('Highscore', font = 'Franklin 20', key='highscore_button')]
+        ]
+            
+    return layout_startsida
 
-        window.close()
-        
-    
-    
+def layout_spel(fs, col1, col2, col3):
+    layout_spelsida = [
+    [sg.Input(key = 'gissning' , font = 'Franklin 20'), 
+    sg.Button('gissa', font = 'Franklin 20', key='gissa_button')],
+    [sg.Text("...", key= 'varning', font = fs)],
+    [sg.Column(col1 , element_justification='c'), 
+    sg.Column(col2, element_justification='c'), 
+    sg.Column(col3, element_justification= 'c')]
+        ]
+            
+    return layout_spelsida
+
+
+def layout_vinst():
+    layout_vinstsida = [
+    [sg.Text('Grattis! du vann!', font = 'Franklin 26') ],
+    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
+    sg.Button('Ja!', key ='kör_igen_button', font = 'Franklin 26')],
+        ]
+            
+    return layout_vinstsida
+
+def layout_max(fs):
+    layout_maxgissningar = [
+    [sg.Text('Du har max 5 gissningar', font = fs)],   
+    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
+    sg.Button('Ja!', key ='kör_igen2_button', font = 'Franklin 26')],
+        ]
+            
+    return layout_maxgissningar
+
+
+def layout_high(fs, col4, col5):
+    layout_highscorelista = [
+    [sg.Text('Highscore', font = fs), sg.Button('Tillbaka till startsidan', key ='tillbaka_button', font= 'Frankline 20' )],
+    [sg.Column(col4, element_justification=('c')), sg.Column(col5, element_justification=('c'))]
+        ]
+            
+    return layout_highscorelista
    
--- sg.py
+++ sg.py
@@ -5,13 +5,6 @@ import SwedishWordle
 from highscorelista import *
 from layout import *
 
-
-
-#Flytta ut onödig info
-
-#Flytta layout info till egen .py
-
-
 #listor
 highscore_file_path = 'highscore.json'
 highscore = []
@@ -20,65 +13,24 @@ highscore_gissningar = []
 highscore_ord = []
 
 
-sg.theme('Reddit')
-sz=(20,30) #size
-fs = 'Frankline 20' #fontsize
-tc= 'white' #text color
-
-#kolummner
-col1=[[sg.Text('Ledtråd', font = fs)],
-[sg.Text( '...', key = 'lista', text_color = tc, justification = 'center', font = 'Franklin 20', background_color='blue', size=sz)]]
-col2=[[sg.Text('Ord', font = fs)],
-[sg.Text('...', key ='ord', text_color = tc, justification = 'center', font = 'Franklin 20', background_color='blue', size=sz)]]
-col3=[[sg.Text('Antal gissningar', font = fs)],
-[sg.Text('...',  key = 'antal', text_color = tc, justification = 'center', font = 'Franklin 50', background_color='blue', size = (20,5))]]
-col4=[[sg.Text('Vinstgissning:', font = fs)],[sg.Text('...', key = 'rätt_ord', text_color = tc, justification = 'center', font = fs, background_color='blue', size = (15,20))]]
-col5=[[sg.Text('Antal gissningar', font = fs)],[sg.Text('...', key = 'score', text_color = tc, justification = 'center', font = fs, background_color='blue', size = (15,20))]]
+sz, fs, tc = design()
+col1, col2, col3, col4, col5 = columner(sz, fs, tc)
 
 #layout
-
-layout_startsida = [
-    [sg.Text('Hej och välkomna till Wordle!', font = 'Franklin 26', justification = 'center')],
-    [sg.Text('Spelet går ut på att du ska försöka gissa vilket ord med fem bokstäver som jag tänker på! \nNär du har gissat på ett ord så kan du se vilket eller vilka bokstäver som finns i det riktiga ordet. \n0 betyder att bokstaven inte finns i ordet\n1 betyder att bokstaven är rätt men på fel plats \n2 betyder att bokstaven är rätt och på rätt plats', font = 'Franklin 17', justification = 'center')],
-    [sg.Text('Vill du starta ett nytt spel?', font = 'Franklin 20', justification = 'center'), 
-    sg.Button('Ja, starta nytt spel', font = 'Franklin 20', key='starta_spel_button'), 
-    sg.Button('Highscore', font = 'Franklin 20', key='highscore_button')]
-        ]
-        
-layout_spelsida = [
-    [sg.Input(key = 'gissning' , font = 'Franklin 20'), 
-    sg.Button('gissa', font = 'Franklin 20', key='gissa_button')],
-    [sg.Text("...", key= 'varning', font = fs)],
-    [sg.Column(col1 , element_justification='c'), 
-    sg.Column(col2, element_justification='c'), 
-    sg.Column(col3, element_justification= 'c')]
-        ]
-
-layout_vinstsida = [
-    [sg.Text('Grattis! du vann!', font = 'Franklin 26') ],
-    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
-    sg.Button('Ja!', key ='kör_igen_button', font = 'Franklin 26')],
-        ]
-
-layout_maxgissningar = [
-    [sg.Text('Du har max 5 gissningar', font = fs)],   
-    [sg.Text('Vill du spela igen?', font = 'Franklin 26'), 
-    sg.Button('Ja!', key ='kör_igen2_button', font = 'Franklin 26')],
-        ]
-
-layout_highscorelista = [
-    [sg.Text('Highscore', font = fs), sg.Button('Tillbaka till startsidan', key ='tillbaka_button', font= 'Frankline 20' )],
-    [sg.Column(col4, element_justification=('c')), sg.Column(col5, element_justification=('c'))]
-        ]
-    
-
-layout = [[sg.Column(layout_startsida, key = 'COL1'), sg.Column(layout_spelsida, visible = False, key = 'COL2'), sg.Column(layout_vinstsida, visible = False, key = 'COL3'), sg.Column(layout_maxgissningar, visible = False, key = 'COL4'),  sg.Column(layout_highscorelista, visible = False, key = 'COL5')]]
-
-window = sg.Window('Wordle', layout, size=(800,400))
-
-
+window = layout_all(fs, col1, col2, col3, col4, col5)
 layout = 1
 
+def update_window(layout, window):
+    window[f'COL{layout}'].update(visible = False)
+    layout = layout + 1
+    window[f'COL{layout}'].update(visible = True)
+    return layout
+
+def update_window2(window, layout):
+    window[f'COL{layout}'].update(visible = False)
+    layout = 1
+    window[f'COL{layout}'].update(visible = True)
+    return layout
 
 while True:
     event, values = window.read()
@@ -88,9 +40,8 @@ while True:
         break
 
     if event == 'kör_igen_button' or event == 'kör_igen2_button':
-        window[f'COL{layout}'].update(visible = False)
-        layout = 1
-        window[f'COL{layout}'].update(visible = True)
+        
+        layout = update_window2(window, layout)
 
     if event == 'starta_spel_button':
         tidigare_gissningar = []
@@ -99,9 +50,7 @@ while True:
 
         #funktion av window och upppdatering av layout
         
-        window[f'COL{layout}'].update(visible = False)
-        layout = layout + 1
-        window[f'COL{layout}'].update(visible = True)
+        layout = update_window(layout, window)
 
         window['lista'].update('')
         window['lista'].update(visible = True)
